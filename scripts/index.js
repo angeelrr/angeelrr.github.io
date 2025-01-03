4a3cb35bca4531b22ba1015cb959d14ec1a2e80e
Ángel Rodríguez <68178624+angeelrr@users.noreply.github.com>
2025-01-03 20:24:59 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Language detection and redirection logic
   const userLang = navigator.language || navigator.userLanguage;
   const langCode = userLang.slice(0, 2); // Get the first two characters
 
-  // Handle language redirection *after* page load
-  if (langCode === 'es' && !window.location.pathname.startsWith('/es')) {
-    window.location.replace('/es' + window.location.pathname);
+  // Check if the page is in the right language and do the redirection only when necessary
+  if (langCode === 'es' && !window.location.pathname.startsWith('/es/')) {
+    // Make sure we only add the "/es" path if we're not already on it
+    window.location.replace('/es' + window.location.pathname.replace('/login', ''));
   } else if (langCode !== 'es' && window.location.pathname.startsWith('/es')) {
+    // Remove "/es" if we are on the Spanish path but the user's language is not Spanish
     window.location.replace(window.location.pathname.replace('/es', ''));
   }
 
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (username === validUsername && password === validPassword) {
       // Redirect to dashboard (replace with your dashboard URL)
-      window.location.href = "../../dashboard/dashboard.html";
+      window.location.href = "/dashboard/dashboard.html";
     } else {
       alert('Invalid credentials. Please try again.'); // Show alert message
       errorMessage.style.display = 'block'; // Show error message
