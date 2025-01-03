d670a2718952f5d46ff0353d74e5673a1ce282e8
Ángel Rodríguez <68178624+angeelrr@users.noreply.github.com>
2025-01-03 16:26:09 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
 document.getElementById('login-form').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent form submission
+
+const userLang = navigator.language || navigator.userLanguage; // e.g., "es-ES" or "en-US"
+if (userLang.startsWith('es')) {
+  // Load Spanish content
+  window.location.href = '/es/index.html';
+} else {
+  // Load English content
+  window.location.href = '/en/index.html';
+}
+
   
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
